e812fa7946396cd0efc292ca7bbce0831fede11f
PabloCacciagioni <152035203+PabloCacciagioni@users.noreply.github.com>
2024-07-03 09:27:53 -0300

    Linking the table

--- go.mod
+++ go.mod
@@ -9,6 +9,8 @@ require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/google/uuid v1.5.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.17.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -18,4 +20,6 @@ require (
 	github.com/valyala/fasthttp v1.51.0 // indirect
 	github.com/valyala/tcplisten v1.0.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
+	gorm.io/driver/mysql v1.5.7
+	gorm.io/gorm v1.25.10 
 )
--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gofiber/fiber/v2"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
+
+type Todo struct {
+	ID          uint   `gorm:"primaryKey;autoIncrement"`
+	Title       string `gorm:"size:255;not null"`
+	Description string `gorm:"type:text"`
+}
 
 func main() {
 	app := fiber.New()
@@ -14,4 +24,12 @@ func main() {
 	})
 
 	app.Listen(":3000")
+
+	dsn := "todouser:todopass@tcp(127.0.0.1:3306)/tododb?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Error connecting to database: %v", err)
+	}
+
+	db.AutoMigrate(&Todo{})
 }
