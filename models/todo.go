4b78c64bccfbcd4567632315dd4114e9f2cd6a1d
Leandro Cacciagioni <leandro.21.2008@gmail.com>
2024-07-26 20:28:08 +0200

    Fixes

--- models/todo.go
+++ models/todo.go
@@ -8,13 +8,36 @@ import (
 type Todo struct {
 	gorm.Model
 
-	ID          uint   `json:"id" gorm:"primaryKey"`
-	Title       string `json:"title" validate:"required,min=3,max=100"`
-	Description string `json:"description" validate:"required,max=1000"`
+	ID          uint64 `json:"id" gorm:"primaryKey"`
+	Title       string `json:"title" gorm:"index:idx_todo_title,unique" validate:"required,min=3,max=100"`
+	Description string `json:"description" validate:"max=1000"`
 }
 
-var validate = validator.New()
-
 func (t *Todo) Validate() error {
+	validate := validator.New()
 	return validate.Struct(t)
+}
+
+// GetTodo returns the requested Todo or error
+func GetTodo(id uint64, db *gorm.DB) (todo *Todo, err error) {
+	todo = &Todo{ID: id}
+	err = db.First(todo).Error
+	return todo, err
+}
+
+func ListTodos(db *gorm.DB) (todos []Todo, err error) {
+	err = db.Find(&todos).Error
+	return todos, err
+}
+
+func (t *Todo) Create(db *gorm.DB) (err error) {
+	return db.Create(t).Error
+}
+
+func (t *Todo) Update(db *gorm.DB) (err error) {
+	return db.Save(t).Error
+}
+
+func (t *Todo) Delete(db *gorm.DB) (err error) {
+	return db.Delete(t).Error
 }
---
+++ models/todo_test.go
@@ -0,0 +1,139 @@
+package models_test
+
+import (
+	"testing"
+
+	"github.com/PabloCacciagioni/project_golang/database"
+	"github.com/PabloCacciagioni/project_golang/models"
+	"github.com/brianvoe/gofakeit/v7"
+)
+
+func TestValidation(t *testing.T) {
+	todo := models.Todo{
+		Title:       "My TODO" + gofakeit.UUID(),
+		Description: gofakeit.LoremIpsumSentence(10),
+	}
+
+	// Here we check that by default the above thing shold pass
+	if todo.Validate() != nil {
+		t.Errorf("Expected todo to be valid, but got invalid with error: %v\n", todo.Validate())
+	}
+
+	todo = models.Todo{
+		Title:       "",
+		Description: gofakeit.LoremIpsumSentence(10),
+	}
+
+	if todo.Validate() == nil {
+		t.Error("Expected todo to be invalid")
+	}
+}
+
+func TestCreateTodo(t *testing.T) {
+	// Ensure db is initialized
+	db := database.ConnectDb()
+
+	todo := models.Todo{
+		Title:       gofakeit.LoremIpsumWord() + gofakeit.UUID(),
+		Description: gofakeit.LoremIpsumSentence(5),
+	}
+
+	err := todo.Create(db)
+	if err != nil {
+		t.Errorf("`todo.Create()` was expected to succeed but failed with err: %v\n", err)
+	}
+
+	err = todo.Create(db)
+	if err == nil {
+		t.Error("`todo.Create()` was expected to fail because of duplicated title")
+	}
+}
+
+func TestUpdateTodo(t *testing.T) {
+	// Ensure db is initialized
+	db := database.ConnectDb()
+
+	todo := models.Todo{
+		Title:       gofakeit.LoremIpsumWord() + gofakeit.UUID(),
+		Description: gofakeit.LoremIpsumSentence(5),
+	}
+
+	err := todo.Create(db)
+	if err != nil {
+		t.Errorf("`todo.Create()` was expected to succeed but failed with err: %v\n", err)
+	}
+
+	todo.Title = gofakeit.LoremIpsumWord() + gofakeit.UUID()
+	err = todo.Update(db)
+	if err != nil {
+		t.Errorf("`todo.Update()` was expected to succeed but failed with err: %v\n", err)
+	}
+}
+
+func TestDeleteTodo(t *testing.T) {
+	// Ensure db is initialized
+	db := database.ConnectDb()
+
+	todo := models.Todo{
+		Title:       gofakeit.LoremIpsumWord() + gofakeit.UUID(),
+		Description: gofakeit.LoremIpsumSentence(5),
+	}
+
+	err := todo.Create(db)
+	if err != nil {
+		t.Errorf("`todo.Create()` was expected to succeed but failed with err: %v\n", err)
+	}
+
+	err = todo.Delete(db)
+	if err != nil {
+		t.Errorf("`todo.Delete()` was expected to succeed but failed with err: %v\n", err)
+	}
+}
+
+func TestGetTodo(t *testing.T) {
+	// Ensure db is initialized
+	db := database.ConnectDb()
+
+	todo := models.Todo{
+		Title:       gofakeit.LoremIpsumWord() + gofakeit.UUID(),
+		Description: gofakeit.LoremIpsumSentence(5),
+	}
+
+	err := todo.Create(db)
+	if err != nil {
+		t.Errorf("`todo.Create()` was expected to succeed but failed with err: %v\n", err)
+	}
+
+	anotherTodo, err := models.GetTodo(todo.ID, db)
+	if err != nil {
+		t.Errorf("`models.GetTodo()` was expected to succeed but failed with err: %v\n", err)
+	}
+
+	if todo != *anotherTodo {
+		t.Error("both todos were expected to be equal")
+	}
+}
+
+func TestListTodo(t *testing.T) {
+	// Ensure db is initialized
+	db := database.ConnectDb()
+
+	todo := models.Todo{
+		Title:       gofakeit.LoremIpsumWord() + gofakeit.UUID(),
+		Description: gofakeit.LoremIpsumSentence(5),
+	}
+
+	err := todo.Create(db)
+	if err != nil {
+		t.Errorf("`todo.Create()` was expected to succeed but failed with err: %v\n", err)
+	}
+
+	allTodos, err := models.ListTodos(db)
+	if err != nil {
+		t.Errorf("`models.ListTodos()` was expected to succeed but failed with err: %v\n", err)
+	}
+
+	if len(allTodos) == 0 {
+		t.Error("there should be at least one todo that we just created")
+	}
+}
