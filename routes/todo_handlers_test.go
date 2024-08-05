2828c9071b83ed18ae3cbbc2c2c2b9dfbd74bfd0
PabloCacciagioni <152035203+PabloCacciagioni@users.noreply.github.com>
2024-08-05 20:42:15 -0300

    Corrections1

--- routes/todo_handlers_test.go
+++ routes/todo_handlers_test.go
@@ -1,60 +1,67 @@
 package routes_test
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http/httptest"
 	"strconv"
 	"strings"
 	"testing"
 
+	"github.com/brianvoe/gofakeit/v7"
 	"github.com/gofiber/fiber/v2"
 	"github.com/stretchr/testify/assert"
 
-	"github.com/PabloCacciagioni/project_golang/database"
 	"github.com/PabloCacciagioni/project_golang/models"
 	"github.com/PabloCacciagioni/project_golang/routes"
 )
 
+var createdTodoID uint64
+
 func SetupApp() *fiber.App {
 	app := fiber.New()
-	db := database.ConnectDb()
-
-	app.Use(func(c *fiber.Ctx) error {
-		c.Locals("db", db)
-		return c.Next()
-	})
-
 	routes.SetupRoutes(app)
 
 	return app
 }
 
+func TestAddTodo_Succes(t *testing.T) {
+	app := SetupApp()
+
+	title := fmt.Sprintf(`{"title":"My TODO %s"}`, gofakeit.UUID())
+	req := httptest.NewRequest("POST", "/todos", strings.NewReader(title))
+	req.Header.Set("Content-type", "application/json")
+	resp, _ := app.Test(req)
+
+	assert.Equal(t, fiber.StatusCreated, resp.StatusCode)
+
+	var todo models.Todo
+	json.NewDecoder(resp.Body).Decode(&todo)
+	createdTodoID = todo.ID
+}
+
 func TestListTodos(t *testing.T) {
 	app := SetupApp()
+
+	assert.NotZero(t, createdTodoID, "The todo ID should not be zero. Make sure TestAddTodo_Success runs before this test.")
 
 	req := httptest.NewRequest("GET", "/todos", nil)
 	resp, _ := app.Test(req)
 
 	assert.Equal(t, fiber.StatusOK, resp.StatusCode)
-}
 
-func TestAddTodo_Succes(t *testing.T) {
-	app := SetupApp()
+	var todos []models.Todo
+	json.NewDecoder(resp.Body).Decode(&todos)
 
-	req := httptest.NewRequest("POST", "/todos", strings.NewReader(`{"title":"Test Todo"}`))
-	req.Header.Set("Content-type", "application/json")
-	resp, _ := app.Test(req)
-
-	assert.Equal(t, fiber.StatusCreated, resp.StatusCode)
+	assert.Greater(t, len(todos), 0)
 }
 
 func TestGetTodo_Succes(t *testing.T) {
 	app := SetupApp()
 
-	db := database.ConnectDb()
-	todo := models.Todo{Title: "Test Todo"}
-	db.Create(&todo)
+	assert.NotZero(t, createdTodoID, "The todo ID should not be zero. Make sure TestAddTodo_Success runs before this test.")
 
-	req := httptest.NewRequest("GET", "/todos/"+strconv.Itoa(int(todo.ID)), nil)
+	req := httptest.NewRequest("GET", "/todos/"+strconv.Itoa(int(createdTodoID)), nil)
 	resp, _ := app.Test(req)
 
 	assert.Equal(t, fiber.StatusOK, resp.StatusCode)
@@ -63,11 +70,9 @@ func TestGetTodo_Succes(t *testing.T) {
 func TestUpdateTodo_Succes(t *testing.T) {
 	app := SetupApp()
 
-	db := database.ConnectDb()
-	todo := models.Todo{Title: "Test Todo"}
-	db.Create(&todo)
+	assert.NotZero(t, createdTodoID, "The todo ID should not be zero. Make sure TestAddTodo_Success runs before this test.")
 
-	req := httptest.NewRequest("PUT", "/todos/"+strconv.Itoa(int(todo.ID)), strings.NewReader(`{"title":"Updated Todo"}`))
+	req := httptest.NewRequest("PUT", "/todos/"+strconv.Itoa(int(createdTodoID)), strings.NewReader(`{"title":"Updated Todo"}`))
 	req.Header.Set("Content-Type", "application/json")
 	resp, _ := app.Test(req)
 
@@ -77,11 +82,9 @@ func TestUpdateTodo_Succes(t *testing.T) {
 func TestDeleteTodo_Succes(t *testing.T) {
 	app := SetupApp()
 
-	db := database.ConnectDb()
-	todo := models.Todo{Title: "Test Todo"}
-	db.Create(&todo)
+	assert.NotZero(t, createdTodoID, "The todo ID should not be zero. Make sure TestAddTodo_Success runs before this test.")
 
-	req := httptest.NewRequest("DELETE", "/todos/"+strconv.Itoa(int(todo.ID)), nil)
+	req := httptest.NewRequest("DELETE", "/todos/"+strconv.Itoa(int(createdTodoID)), nil)
 	resp, _ := app.Test(req)
 
 	assert.Equal(t, fiber.StatusOK, resp.StatusCode)
